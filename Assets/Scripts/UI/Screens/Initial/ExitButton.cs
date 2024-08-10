38e7e039684823741f03652fcf51fd8dd4e6caf3
Stanislav Anishchenko <dpobocek12@gmail.com>
2024-08-10 21:57:15 +0500

    Added ExitButton. Increased project version.

---
+++ Assets/Scripts/UI/Screens/Initial/ExitButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Screens.Initial
+{
+    public class ExitButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(Exit);
+        }
+
+        private void Exit()
+        {
+            if (!Application.isEditor)
+                Application.Quit();
+        }
+    }
+}
