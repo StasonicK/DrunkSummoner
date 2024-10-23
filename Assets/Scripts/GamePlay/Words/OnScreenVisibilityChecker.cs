89914a0a248d8cd704367650f8a8a03f0626e196
Stanislav Anishchenko <dpobocek12@gmail.com>
2024-10-23 17:50:08 +0500

    Finished. Fixed out of screen appearance of a word bug.

--- Assets/Scripts/GamePlay/Words/OnScreenVisibilityChecker.cs
+++ Assets/Scripts/GamePlay/Words/OnScreenVisibilityChecker.cs
@@ -5,15 +5,21 @@ namespace GamePlay.Words
 {
     public class OnScreenVisibilityChecker : MonoBehaviour
     {
-        [SerializeField] private float _lowerEdge = -0.1f;
-        [SerializeField] private float _upperEdge = 1.1f;
+        // [SerializeField] private float _startAppearEdge = -0.1f;
+        // [SerializeField] private float _endAppearEdge = 1.1f;
+        [SerializeField] private float _startDisappearEdge = -0.15f;
+        [SerializeField] private float _endDisappearEdge = 1.15f;
+        [SerializeField] private float _disappearToAppearDifferance = 0.05f;
 
         private const int AFTER_MOVE_TO_OPPOSITE_SCREEN_EDGE_DELAY = 500;
 
         private Camera _mainCamera;
-        private Vector3 _viewportPosition;
-        private Vector3 _cameraViewportToWorldPoint;
-        private Vector3 _newPosition;
+        private float _newXPosition;
+        private float _newYPosition;
+        private Vector3 _viewportPosition = Vector3.zero;
+        private Vector3 _newPosition = Vector3.zero;
+        private float _endAppearEdge;
+        private float _startAppearEdge;
 
         private void Start() =>
             _mainCamera = Camera.main;
@@ -26,36 +32,36 @@ namespace GamePlay.Words
 
         private async void CheckScreenEdge()
         {
-            if (_viewportPosition.x < _lowerEdge || _viewportPosition.x > _upperEdge ||
-                _viewportPosition.y < _lowerEdge ||
-                _viewportPosition.y > _upperEdge)
+            if (_viewportPosition.x < _startDisappearEdge || _viewportPosition.x > _endDisappearEdge ||
+                _viewportPosition.y < _startDisappearEdge ||
+                _viewportPosition.y > _endDisappearEdge)
             {
                 MoveToOppositeSide();
-                await UniTask.Yield(); // Ensure the position update is processed before the delay
+                await UniTask.Yield();
                 await UniTask.Delay(AFTER_MOVE_TO_OPPOSITE_SCREEN_EDGE_DELAY);
             }
         }
 
         private void MoveToOppositeSide()
         {
-            Debug.Log("MoveToOppositeScreenEdge");
+            // Debug.Log("MoveToOppositeSide start");
             _newPosition = transform.localPosition;
 
-            if ((_viewportPosition.x < 0 && _viewportPosition.y < 0) ||
-                (_viewportPosition.x > 1 && _viewportPosition.y < 0) ||
-                (_viewportPosition.x < 0 && _viewportPosition.y > 1) ||
-                (_viewportPosition.x > 1 && _viewportPosition.y > 1))
+            if ((_viewportPosition.x < _startDisappearEdge && _viewportPosition.y < _startDisappearEdge) ||
+                (_viewportPosition.x > _endDisappearEdge && _viewportPosition.y < _startDisappearEdge) ||
+                (_viewportPosition.x < _startDisappearEdge && _viewportPosition.y > _endDisappearEdge) ||
+                (_viewportPosition.x > _endDisappearEdge && _viewportPosition.y > _endDisappearEdge))
             {
-                _newPosition.x = -_newPosition.x;
-                _newPosition.y = -_newPosition.y;
+                _newPosition.x *= -.95f;
+                _newPosition.y *= -.95f;
             }
-            else if (_viewportPosition.x < 0 || _viewportPosition.x > 1)
+            else if (_viewportPosition.x < _startDisappearEdge || _viewportPosition.x > _endDisappearEdge)
             {
-                _newPosition.x = -_newPosition.x;
+                _newPosition.x *= -.95f;
             }
-            else if (_viewportPosition.y < 0 || _viewportPosition.y > 1)
+            else if (_viewportPosition.y < _startDisappearEdge || _viewportPosition.y > _endDisappearEdge)
             {
-                _newPosition.y = -_newPosition.y;
+                _newPosition.y *= -.95f;
             }
 
             transform.localPosition = _newPosition;
